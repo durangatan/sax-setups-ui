e9573badbde1d6340333206fd1b0fce857bcdaee
durangatan <joeduran8@gmail.com>
2018-10-14 16:31:57 -0500

    adds footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    contact <span>admin at saxsetups dot com</span> to contribute a setup!
+  </footer>
+</template>
+
+<script>
+import SetupList from "./SetupList.vue";
+export default {
+  name: "Footer"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+footer {
+  width: 100%;
+  padding: 30px;
+  background-color: #4dba87;
+  color: #ffffff;
+  margin-top: 30px;
+}
+span {
+  font-weight: bold;
+}
+</style>
