fce0c768ba247166f97b81376e29e712c3511064
durangatan <joeduran8@gmail.com>
2018-10-15 22:12:49 -0500

    search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="search">
+        <input type="text" 
+          v-bind:value="value"      
+          v-on:input="$emit('input', $event.target.value)">
+        <button v-on:click="$emit('clear-filter')">ⓧ</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  props: {
+    value: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.search {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  input {
+    font-size: 24px;
+    z-index: 10;
+    width: 80%;
+  }
+  button {
+    
+    padding: 9px;
+    background: transparent;
+    border: none;
+    margin-left: -32px;
+    z-index: 12;
+    cursor: pointer;
+  }
+}
+</style>
