38dddfb6cd78bd2e2b1e314114dafdaaed03078a
durangatan <joeduran8@gmail.com>
2018-10-18 22:51:15 -0500

    add loading spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-bind:class="{ spin: $apollo.loading > 0 }">🎷</div>
+</template>
+
+<script>
+export default {
+  name: 'Spinner'
+};
+</script>
+
+<style scoped lang="scss">
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+div {
+  .spin {
+    animation: spin 1s infinite;
+  }
+}
+</style>
